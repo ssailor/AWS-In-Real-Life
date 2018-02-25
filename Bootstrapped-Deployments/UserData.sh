032014bf4a3a0a2fbccdc66d717c217322bb5b98
Stephen Sailor <ssailor90@gmail.com>
2018-02-25 00:21:42 -0500

    Updated Bootstrapped-Deployments to incorporate AutoScaling LifeCycle hooks

--- Bootstrapped-Deployments/UserData.sh
+++ Bootstrapped-Deployments/UserData.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+
+# Gain root access
+sudo su
+
+# Script Variables
+BucketName="INSERT BUCKET NAME HERE"
+LifeCycleHookName="INSERT LIFECYCLE-HOOK-NAME HERE"
+AutoScalingGroupName="INSERT AUTOSCALING GROUP NAME HERE"
+Region="INSERT REGION HERE"
+
+# Dynamically get the Instance ID from the Instance
+InstanceID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+
+# Get Updates
 yum update -y
+
 # Install the apache web server
 yum install -y httpd
+
 # Change directory to apache website directory
 cd /var/www/html/
+
 # AWS Cli - Copy the deployment package from S3 
-sudo aws s3 cp s3://{INSERT-BUCKET-NAME-HERE}/latest/index.zip ./index.zip
+aws s3 cp s3://$BucketName/latest/index.zip ./index.zip
+
 # Unzip the code package to the current directory
-sudo unzip index.zip -d .
+unzip index.zip -d .
+
 # Remove the zip file
-sudo rm -f index.zip
+rm -f index.zip
+
 # Start the Web Server
 service httpd start
+
+# Trigger the Autoscaling hook to complete and release the instance into service
+aws autoscaling complete-lifecycle-action --lifecycle-hook-name "$LifeCycleHookName" --auto-scaling-group-name "$AutoScalingGroupName" --lifecycle-action-result CONTINUE --instance-id "$InstanceID" --region "$Region"
